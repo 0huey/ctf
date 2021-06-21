2bc480435b5994f4b4a0fc83bf41f15995fb5aad
0huey <34045167+0huey@users.noreply.github.com>
2021-06-20 20:37:41 -0600

    update provision

--- provision.sh
+++ provision.sh
@@ -1,13 +1,9 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-#apt-get upgrade -y
-apt-get install -y firewalld ncat gdb gdbserver python3-pip strace openjdk-11-jdk openjdk-11-jre gradle
-#apt-get autoremove -y
+apt-get install -y firewalld ncat gdb gdbserver python3-pip strace openjdk-11-jdk openjdk-11-jre
 
-pip3 install pwntools ROPGadget
-
-sudo -u vagrant mkdir -p /home/vagrant/Downloads
+pip3 install pwntools ROPGadget keystone-engine ropper
 
 GHIDRA_VERSION=$(curl -Ls https://ghidra-sre.org/ | grep -oE "ghidra_.*zip" | head -n 1)
 echo Downloading ${GHIDRA_VERSION}
@@ -18,12 +14,16 @@ sudo -u vagrant unzip -q ghidra.zip
 
 sudo -u vagrant sh -c "$(curl -fsSL http://gef.blah.cat/sh)"
 
+gem install seccomp-tools
+
 echo Setting up firewalld
 systemctl --now enable firewalld
 firewall-cmd --permanent --new-zone=vpn
 firewall-cmd --permanent --zone=vpn --add-interface=tun0
 firewall-cmd --permanent --zone=public --add-interface=eth0
 firewall-cmd --reload
+
+echo "PATH=$PATH:/home/vagrant/.local/bin" >> /home/vagrant/.bashrc
 
 if [[ -f /vagrant/hackthebox/hackthebox.conf ]]; then
     cp /vagrant/hackthebox/hackthebox.conf /etc/openvpn/client/
