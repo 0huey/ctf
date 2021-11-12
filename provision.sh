027aef1eb36c8f5e55abe89bd8197e4772933ed5
0huey <34045167+0huey@users.noreply.github.com>
2021-11-12 11:13:54 -0700

    update provision ghidra

--- provision.sh
+++ provision.sh
@@ -1,15 +1,14 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y firewalld ncat gdb gdbserver python3-pip strace openjdk-11-jdk openjdk-11-jre
+apt-get install -y firewalld ncat gdb gdbserver jq python3-pip strace openjdk-11-jdk openjdk-11-jre
 
 pip3 install pwntools ROPGadget keystone-engine ropper
 
 sudo -u vagrant mkdir /home/vagrant/Downloads
 
-GHIDRA_VERSION=$(curl -Ls https://ghidra-sre.org/ | grep -oE "ghidra_.*zip" | head -n 1)
-echo Downloading ${GHIDRA_VERSION}
-wget -q -O /home/vagrant/Downloads/ghidra.zip https://ghidra-sre.org/${GHIDRA_VERSION}
+echo Downloading Ghidra
+curl -s https://api.github.com/repos/NationalSecurityAgency/ghidra/releases/latest | jq .assets[0].browser_download_url | tr -d '"' | wget -qO /home/vagrant/Downloads/ghidra.zip -i -
 
 sudo -u vagrant unzip -q /home/vagrant/Downloads/ghidra.zip -d /home/vagrant/Downloads
 
@@ -29,3 +28,5 @@ echo "PATH=$PATH:/home/vagrant/.local/bin" >> /home/vagrant/.bashrc
 if [[ -f /vagrant/hackthebox/hackthebox.conf ]]; then
     cp /vagrant/hackthebox/hackthebox.conf /etc/openvpn/client/
 fi
+
+updatedb
