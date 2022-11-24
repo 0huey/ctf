901962e821e29fb667fdbc228587842b7f97f7d9
0huey <34045167+0huey@users.noreply.github.com>
2022-11-23 23:53:02 -0700

    add factorial header file

--- misc/large-factorials/multi-threaded/factorial.c
+++ misc/large-factorials/multi-threaded/factorial.c
@@ -1,43 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <inttypes.h>
-
-#ifdef _WIN32
-
-#include <windows.h>
-#include <time.h>
-#pragma warning(disable: 4996)  //strncpy
-#define _USE_32BIT_TIME_T
-
-VOID CALLBACK FactorialMul(PTP_CALLBACK_INSTANCE instance, PVOID context, PTP_WORK work);
-
-#elif __linux__
-
-#include <pthread.h>
-
-void* FactorialMul(void* context);
-#endif
-
-typedef struct _THREAD_CALLBACK_PARAMS {
-    char* output;
-    char* multiplicand;
-    char* multiplier;
-    size_t iMultiplier;
-} THREAD_CALLBACK_PARAMS;
-
-#define DIGIT_STR_INT_CONV '0'
-#define NUM_PRODUCTS 6
-#define MEM_SIZE 500000
-
-char* factorial(int n);
-void SumProducts(char** products, char* szMultiplicand);
-char DigitStrToInt(char digit);
-char DigitIntToStr(char digit);
-void RevStr(char* str);
-uint64_t TimeNow(void);
-
+#include "factorial.h"
 
 int main(int argc, char** argv) {
     if (argc == 2) {
@@ -95,10 +56,6 @@ char* factorial(int n) {
     SetThreadpoolThreadMaximum(pool, NUM_PRODUCTS);
     SetThreadpoolThreadMinimum(pool, NUM_PRODUCTS);
     SetThreadpoolCallbackPool(&callBackEnviron, pool);
-
-#elif __linux__
-
-
 #endif
 
     //implement long multiplication
