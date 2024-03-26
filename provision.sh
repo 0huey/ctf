78356f1cfd770c97fc4cc1f62f45f7b84d977331
0huey <34045167+0huey@users.noreply.github.com>
2024-03-26 15:57:07 -0600

    update software provision

--- provision.sh
+++ provision.sh
@@ -1,13 +1,14 @@
 export DEBIAN_FRONTEND=noninteractive
 
+set -x
+
 apt-get update
-apt-get install -y firewalld ncat gdb gdbserver jq python3-pip strace openjdk-17-jdk openjdk-17-jre
+apt-get install -y firewalld ncat gdb gdbserver jq python3-pip strace openjdk-17-jdk openjdk-17-jre geany gobuster
 
 pip3 install pwntools ROPGadget keystone-engine ropper
 
 sudo -u vagrant mkdir /home/vagrant/Downloads
 
-echo Downloading Ghidra
 curl -s https://api.github.com/repos/NationalSecurityAgency/ghidra/releases/latest | jq .assets[0].browser_download_url | tr -d '"' | wget -qO /home/vagrant/Downloads/ghidra.zip -i -
 
 sudo -u vagrant unzip -q /home/vagrant/Downloads/ghidra.zip -d /home/vagrant/Downloads
@@ -16,7 +17,6 @@ sudo -u vagrant sh -c "$(curl -fsSL http://gef.blah.cat/sh)"
 
 gem install seccomp-tools
 
-echo Setting up firewalld
 systemctl --now enable firewalld
 firewall-cmd --permanent --new-zone=vpn
 firewall-cmd --permanent --zone=vpn --add-interface=tun0
